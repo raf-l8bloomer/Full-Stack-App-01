7e1f28a84ccdb6f295949a6af093f4b180e92135
rafael.lumang <lumang.rafael@gmail.com>
2023-09-06 14:30:09 -0700

    Changed a tag to Link from React Router Dom

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,10 +1,27 @@
 /** retrieves course details from /api/courses/:id
- * renders "Delete Course" that sends a delete request
+ /* renders Delete Course that sends a delete request
  * also renders "Update Course" to navigate to Update Course Screen
 */
 
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
 const CourseDetail = () => {
-    const [course, setCourse] = useState([]);
+    const courseId = useParams();
+    const [course, setCourse] = useState(null);
+
+    useEffect(() => {
+        axios.get(`http://localhost:5000/api/courses/${courseId}`)
+            .then(response => {
+                setCourse(response.data)
+                // handle success
+            })
+            .catch(error => {
+                // handle error
+                console.log("Error fetching and parsing data", error);
+            })
+    }, [courseId]);
 
 }
 
--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -5,6 +5,7 @@
 
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom"
 
 const Courses = () => {
     const [courses, setCourses] = useState([]);
@@ -28,18 +29,20 @@ const Courses = () => {
     return (
         <>
             {courses.map((course) => {
-                <a class="course--module course--link" href={course.detail}>
+                <Link to={`courses/${course.id}`} class="course--module course--link" key={course.id}>
                     <h2 class="course--label">Course</h2>
                     <h3 class="course--title">{course.title}</h3>
-                </a>
+                </Link>
             })}
-            <a class="course--module course--add--module" href="create-course.html">
-                    <span class="course--add--title">
-                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                        viewBox="0 0 13 13" class="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
-                        New Course
-                    </span>
-                    </a>
+
+            <Link to="create-course.html" class="course--module course--add--module" >
+                <span class="course--add--title">
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                        viewBox="0 0 13 13" class="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+                    </svg>
+                    New Course
+                </span>
+            </Link>
         </>
     )
 
