ef746c032433aaa9b88df26ff2b083933c2c5863
rafael.lumang <lumang.rafael@gmail.com>
2023-09-06 15:00:33 -0700

    CourseDetail complete

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -4,7 +4,7 @@
 */
 
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 
 const CourseDetail = () => {
@@ -22,6 +22,45 @@ const CourseDetail = () => {
                 console.log("Error fetching and parsing data", error);
             })
     }, [courseId]);
+
+    return (
+        <main>
+            <div className="actions--bar">
+                <div className="wrap">
+                    <Link to={`course/${course.id}/update`} className="button" href="update-course.html">Update Course</Link>
+                    <Link to={`course/${course.id}/delete`} className="button" >Delete Course</Link>
+                    <Link to='/courses' className="button" href="index.html">Return to List </Link>
+                </div>
+            </div>
+
+            <div className="wrap">
+                <h2>Course Detail</h2>
+                <form>
+                    <div className="main--flex">
+                        <div>
+                            <h3 className="course--detail--title">Course</h3>
+                            <h4 className="course--name">{course.name}</h4>
+                            <p>By {course.user.id}</p>
+
+                            <p>{course.description}</p>
+                        </div>
+                        <div>
+                            <h3 className="course--detail--title">Estimated Time</h3>
+                            <p>{course.estimatedTime}</p>
+
+                            <h3 className="course--detail--title">Materials Needed</h3>
+                            <ul className="course--detail--list">
+                                {course.materialsNeeded.map((courseMaterial) => {
+                                    <li>{courseMaterial}</li>
+                                })}
+                            </ul>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </main>
+
+    )
 
 }
 
--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -29,16 +29,16 @@ const Courses = () => {
     return (
         <>
             {courses.map((course) => {
-                <Link to={`courses/${course.id}`} class="course--module course--link" key={course.id}>
-                    <h2 class="course--label">Course</h2>
-                    <h3 class="course--title">{course.title}</h3>
+                <Link to={`courses/${course.id}`} className="course--module course--link" key={course.id}>
+                    <h2 className="course--label">Course</h2>
+                    <h3 className="course--title">{course.title}</h3>
                 </Link>
             })}
 
-            <Link to="create-course.html" class="course--module course--add--module" >
-                <span class="course--add--title">
+            <Link to="create-course.html" className="course--module course--add--module" >
+                <span className="course--add--title">
                     <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                        viewBox="0 0 13 13" class="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+                        viewBox="0 0 13 13" className="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
                     </svg>
                     New Course
                 </span>
